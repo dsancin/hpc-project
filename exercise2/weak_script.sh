965548330c3a6b4483b82ac03b9414ec665ca009
dsancin <98841916+dsancin@users.noreply.github.com>
2023-03-20 14:05:38 +0100

    Add files via upload

---
+++ exercise2/weak_script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#SBATCH --job-name="script"
+#SBATCH --output=log.out
+#SBATCH --partition=EPYC
+#SBATCH --exclusive
+#SBATCH --ntask=1
+#SBATCH --cpus-per-task=64
+#SBATCH --time=02:00:00
+#SBATCH --nodelist=epyc[005]
+
+module load architecture/AMD
+module load openBLAS/0.3.21-omp
+module load mkl
+
+rm gemm_oblas.x
+rm gemm_mkl.x
+
+srun -n 1 make cpu
+
+export OMP_NUM_THREADS=64
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+
+for i in {2000..20000..1000}
+do
+   for j in {1..5}
+   do
+       srun ./gemm_oblas.x $i $i $i >> weak_scalability/single/cores_close_oblas.csv
+       srun ./gemm_mkl.x $i $i $i >> weak_scalability/single/cores_close_mkl.csv
+       echo
+   done
+   echo
+done
