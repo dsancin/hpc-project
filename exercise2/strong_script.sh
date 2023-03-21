39a0e3a48286e13f72d93cf1ae6e4e42b1b37a42
dsancin <98841916+dsancin@users.noreply.github.com>
2023-03-22 00:17:57 +0100

    Add files via upload

---
+++ exercise2/strong_script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --job-name="script"
+#SBATCH --output=log.out
+#SBATCH --partition=EPYC
+#SBATCH	--nodes=1
+#SBATCH --exclusive
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
+#SBATCH --time=02:00:00
+#SBATCH --nodelist=epyc[005]
+
+module load architecture/AMD
+module load openBLAS/0.3.21-omp
+module load mkl
+
+rm gemm_oblas.x
+rm gemm_mkl.x
+
+srun -n 1 make cpu
+
+
+export OMP_PLACES=sockets
+export OMP_PROC_BIND=spread
+
+for i in {1, 2, 4, 6, 8, 12, 16, 24, 32, 48, 64}
+do
+   for j in {1..5}
+   do
+       export OMP_NUM_THREADS=$i
+       srun ./gemm_oblas.x 15000 15000 15000 >> strong_scalability/single/sockets_spread_oblas.csv
+       srun ./gemm_mkl.x 15000 15000 15000 >> strong_scalability/single/sockets_spread_mkl.csv
+       echo
+   done
+   echo
+done
