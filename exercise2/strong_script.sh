a33f1a045e15a3fa9266dc505f9a1eb15e41cf13
Davide Sancin <dsancin@login02.hpc>
2023-03-26 14:09:13 +0200

    update0

--- exercise2/strong_script.sh
+++ exercise2/strong_script.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
 #SBATCH --time=02:00:00
-#SBATCH --nodelist=epyc[005]
+#SBATCH --nodelist=epyc[002]
 
 module load architecture/AMD
 module load openBLAS/0.3.21-omp
@@ -24,11 +24,11 @@ for i in 1 2 4 6 8 12 16 24 32 48 64
 do
    export OMP_NUM_THREADS=$i
    export OMP_PLACES=sockets
-   export OMP_PROC_BIND=spread
+   export OMP_PROC_BIND=close
    for j in {1..5}
    do
-       srun ./gemm_oblas.x 15000 15000 15000 >> strong_scalability/single/sockets_spread_oblas.csv
-       srun ./gemm_mkl.x 15000 15000 15000 >> strong_scalability/single/sockets_spread_mkl.csv
+       srun ./gemm_oblas.x 15000 15000 15000 >> strong_scalability/single/sockets_close_oblas.csv
+       srun ./gemm_mkl.x 15000 15000 15000 >> strong_scalability/single/sockets_close_mkl.csv
        echo
    done
    echo
